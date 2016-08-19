afa9303bc07d36eddcfc555c3559649e3f8d234a
Tyler Kell <tkell@sae.au>
2016-08-19 13:20:15 +1000

    Transferred literally everything. Over 2000 files. Fml

---
+++ Assets/buildingplacement/BuildingPlacement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingPlacement : MonoBehaviour {
+    protected SelectionManager selectionManager;
+
+    public GameObject BuildingPrefab;
+
+    public Material InvalidMaterial;
+    public Material ValidMaterial;
+
+    public MeshRenderer meshRenderer;
+    private bool canPlace = true;
+
+    // Use this for initialization
+    void Start () {
+        selectionManager = FindObjectOfType<SelectionManager>();
+        meshRenderer.material = ValidMaterial;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // is the selection manager in placement mode?
+	    if (selectionManager.inPlacementMode)
+        {
+            // move the building to the mouse location
+            transform.position = selectionManager.mousePosition;
+
+            if (canPlace && Input.GetMouseButtonDown(0))
+            {
+                // create the actual building
+                Instantiate(BuildingPrefab, transform.position, transform.rotation);
+
+                // disable placement mode
+                selectionManager.inPlacementMode = false;
+
+                // destroy the placement building
+                Destroy(gameObject);
+            }
+        }
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Building"))
+        {
+            canPlace = false;
+            meshRenderer.material = InvalidMaterial;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Building"))
+        {
+            canPlace = true;
+            meshRenderer.material = ValidMaterial;
+        }
+    }
+}
---
+++ Assets/buildingplacement/Scripts/PlayerUnit.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerUnit : MonoBehaviour {
+    private NavMeshAgent agent;
+    public Vector3 destination;
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+        destination = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agent.SetDestination(destination);
+
+        animator.SetFloat("Speed", Mathf.Max(agent.velocity.magnitude / agent.speed, 0.1f));
+        animator.SetBool("isWalking", agent.velocity.magnitude > 0.1f);
+	}
+
+    public void MoveTo(Vector3 newDestination)
+    {
+        destination = newDestination;
+    }
+}
